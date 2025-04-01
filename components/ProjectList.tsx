cd3a71db63f65d0c574e4b1476bd36ffd989946e
Zakaria <68170594+ZackInfosbd@users.noreply.github.com>
2025-04-01 13:54:38 +0100

    feat: TODO notes for project list

--- components/ProjectList.tsx
+++ components/ProjectList.tsx
@@ -1,4 +1,6 @@
-// TODO: Project list with own components and their sub-componenes
+// TODO: Project list with own components and their sub-componentes
+// TODO: Project list should be in a form of cards
+// TODO: thus shuld contain few features for MVP, as view and edit project and useful infos
 
 const ProjectList = () => {
   return <div>Project List</div>;
